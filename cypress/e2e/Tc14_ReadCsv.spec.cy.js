fc97beeac3e3a8f462140f353f6a81633afb107c
Rasim Aghayev <rasimaqayev@gmail.com>
2024-03-27 00:07:40 +0400

    45. Csv - Reading Data - 7 min

---
+++ cypress/e2e/Tc14_ReadCsv.spec.cy.js
@@ -0,0 +1,29 @@
+const neatCsv = require("neat-csv");
+describe("Csv test", () => {
+  var table;
+  beforeEach(() => {
+    cy.fixture("ConduitCsv.csv")
+      .then(neatCsv)
+      .then((data) => {
+        table = data;
+      });
+  });
+  it("Condouit - Valid Credentials", function () {
+    cy.visit("https://react-redux.realworld.io/"); // visiting the website
+    cy.get('a[href="#login"').click(); // clicking on login button from
+    cy.get('input[placeholder="Email"]').type(table[0].username); // typing email in input field
+    cy.get('input[placeholder="Password"]').type(table[0].password); // typing password in password field
+    cy.get('button[type="submit"]').click(); // submitting the form by clicking on submit button
+    cy.get('a[href="#settings"]').click(); // going to settings page by clicking on setting link
+    cy.get(".btn.btn-outline-danger").click(); // logging out by clicking logout button
+  });
+
+  it("Condouit - Invalid Credentials", function () {
+    cy.visit("https://react-redux.realworld.io/"); // visiting the website
+    cy.get('a[href="#login"').click(); // clicking on login button from
+    cy.get('input[placeholder="Email"]').type(table[1].username); // typing email in input field
+    cy.get('input[placeholder="Password"]').type(table[1].password); // typing password in password field
+    cy.get('button[type="submit"]').click(); // submitting the form by clicking on submit button
+    cy.get(".error-messages").should("contain", "email or password is invalid");
+  });
+});
