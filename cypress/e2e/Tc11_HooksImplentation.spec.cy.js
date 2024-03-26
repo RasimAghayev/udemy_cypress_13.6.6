d7f12b308eb21e940648501db273daf686332ae8
Rasim Aghayev <rasimaqayev@gmail.com>
2024-03-26 23:12:42 +0400

    39. Hooks - Part 2 - 9 min

---
+++ cypress/e2e/Tc11_HooksImplentation.spec.cy.js
@@ -0,0 +1,23 @@
+describe("Hooks Implementation", () => {
+  beforeEach(() => {
+    cy.visit("https://opensource-demo.orangehrmlive.com/");
+    cy.get('input[name="username"]').type("Admin");
+    cy.get('input[name="password"]').type("admin123");
+    cy.get('button[type="submit"]').click();
+  });
+
+  it("Search in Admin Tab with Employee Name", () => {
+    cy.contains("Admin").click();
+    cy.xpath('(//input[@class="oxd-input oxd-input--active"])[2]').type(
+      "Admin"
+    );
+    cy.get('button[type="submit"]').click();
+    cy.get('input[placeholder="Type for hints..."]').type("Admin");
+    cy.get('button[type="submit"]').click();
+  });
+
+  afterEach(() => {
+    cy.get(".oxd-userdropdown-tab").click();
+    cy.contains("Logout").click();
+  });
+});
